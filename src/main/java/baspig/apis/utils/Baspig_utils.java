f46a5ae9059cc60fc283857057f7e80b6cb8c1f9
Badpig Studios <97912584+BadpigStudios@users.noreply.github.com>
2025-03-08 20:38:01 -0600

    small fixing

--- src/main/java/baspig/apis/utils/Baspig_utils.java
+++ src/main/java/baspig/apis/utils/Baspig_utils.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class Baspig_utils implements ModInitializer {
 
-    public static final String MOD_ID = "baspig_utils";
+    public static final String MOD_ID = "assets/baspig_utils";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
