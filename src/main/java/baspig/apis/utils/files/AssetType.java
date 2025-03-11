903960ca7f8397acd7ebde2347d0f5848ecacec6
Badpig Studios <97912584+BadpigStudios@users.noreply.github.com>
2025-03-10 22:49:25 -0600

    Big update -Trim pattern registry -A bunch of new tags -Tab group register fixes -Doc fixes -a lot of small fixes

---
+++ src/main/java/baspig/apis/utils/files/AssetType.java
@@ -0,0 +1,23 @@
+package baspig.apis.utils.files;
+
+/**
+ * UNUSED
+ */
+public enum AssetType {
+    /**
+     * ASSETS type if for general assets folder
+     * TEXTURE type if for texture folder into TEXTURES
+     */
+    ASSETS,
+    TEXTURE,
+
+    /**
+     * DATA, type if for general data folder
+     * RECIPE type if for recipe folder into DATA
+     * TRIM_PATTERN type if for trim_pattern folder into DATA
+     */
+    DATA,
+    RECIPE,
+    TRIM_PATTERN
+
+}
