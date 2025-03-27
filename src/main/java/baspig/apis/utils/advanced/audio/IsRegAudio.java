7c7f23664dba79d378e64bd67381c91a4763b76f
Badpig Studios <97912584+BadpigStudios@users.noreply.github.com>
2025-03-26 20:14:21 -0600

    Fixing 1.21.4 dependency

--- src/main/java/baspig/apis/utils/advanced/audio/IsRegAudio.java
+++ src/main/java/baspig/apis/utils/advanced/audio/IsRegAudio.java
@@ -3,6 +3,7 @@ package baspig.apis.utils.advanced.audio;
 @Deprecated
 @SuppressWarnings(value = "all")
 public class IsRegAudio {
+
     public IsRegAudio(){
         SourceObjectLoader.initiate();
         SoundSourceInitRegister.soundTicker();
