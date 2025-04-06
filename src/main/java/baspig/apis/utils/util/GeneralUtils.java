f74879c48f4152ed73a0b4133e887f1c9a5a17af
Badpig Studios <97912584+BadpigStudios@users.noreply.github.com>
2025-04-06 14:48:41 -0600

    adding stuff

--- src/main/java/baspig/apis/utils/util/GeneralUtils.java
+++ src/main/java/baspig/apis/utils/util/GeneralUtils.java
@@ -10,9 +10,29 @@ public class GeneralUtils {
      * Check the probability against a random float.
      *
      * @param rate The rate to check.
-     * @return True if it's mayor to the random float.
+     * @return True if a random value is higher than the rate value.
      */
     public static Boolean probability(float rate){
+        return (random.nextFloat() * 100 > rate);
+    }
+
+    /**
+     * Check the probability against a random float.
+     *
+     * @param rate The rate to check.
+     * @return True if a random value is lower than the rate value.
+     */
+    public static Boolean probabilityLessThan(float rate){
         return (random.nextFloat() * 100 < rate);
+    }
+
+    /**
+     * Check the probability against a random float.
+     *
+     * @param rate The rate to check.
+     * @return True if a random value is higher than the rate value.
+     */
+    public static Boolean probabilityMoreThan(float rate){
+        return (random.nextFloat() * 100 > rate);
     }
 }
--- src/main/java/baspig/apis/utils/util/block/Offset.java
+++ src/main/java/baspig/apis/utils/util/block/Offset.java
@@ -1,5 +1,7 @@
 package baspig.apis.utils.util.block;
 
+import net.minecraft.util.math.BlockPos;
+
 import java.util.Random;
 
 /**
@@ -10,27 +12,35 @@ import java.util.Random;
  */
 @SuppressWarnings("unused")
 public class Offset {
-
     private final Random random = new Random();
 
-    private final int x;
-    private final int y;
-    private final int z;
+    private final double x;
+    private final double y;
+    private final double z;
+
+    private final double xOffset;
+    private final double yOffset;
+    private final double zOffset;
 
     private final double minRandomOffset;
     private final double maxRandomOffset;
 
-    private boolean hasRandomOffset = false;
+    private final boolean hasRandomOffset;
 
     /**@param x It is the offset parameter on X.
      * @param y It is the offset parameter on Y.
      * @param z It is the offset parameter on Z.*/
     public Offset(int x, int y, int z) {
+        this.hasRandomOffset = false;
         this.minRandomOffset = 0;
         this.maxRandomOffset = 1;
         this.x = x;
         this.y = y;
         this.z = z;
+
+        this.xOffset = 0;
+        this.yOffset = 0;
+        this.zOffset = 0;
     }
 
     /**@param x It is the offset parameter on X.
@@ -41,9 +51,14 @@ public class Offset {
         this.hasRandomOffset = true;
         this.minRandomOffset = 0;
         this.maxRandomOffset = randomOffset;
-        this.x = x;
-        this.y = y;
-        this.z = z;
+
+        this.xOffset = random.nextDouble(maxRandomOffset);
+        this.yOffset = random.nextDouble(maxRandomOffset);
+        this.zOffset = random.nextDouble(maxRandomOffset);
+
+        this.x = x + xOffset;
+        this.y = y + yOffset;
+        this.z = z + zOffset;
     }
 
     /**@param x It is the offset parameter on X.
@@ -60,56 +75,143 @@ public class Offset {
         this.hasRandomOffset = true;
         this.minRandomOffset = minRandomOffset;
         this.maxRandomOffset = maxRandomOffset;
+
+        this.xOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+        this.yOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+        this.zOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+
+        this.x = x + xOffset;
+        this.y = y + yOffset;
+        this.z = z + zOffset;
+    }
+
+    /**@param x It is the offset parameter on X.
+     * @param y It is the offset parameter on Y.
+     * @param z It is the offset parameter on Z.*/
+    public Offset(double x, double y, double z) {
+        this.hasRandomOffset = false;
+        this.minRandomOffset = 0;
+        this.maxRandomOffset = 1;
+
+        this.xOffset = 0;
+        this.yOffset = 0;
+        this.zOffset = 0;
+
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
+    /**@param x It is the offset parameter on X.
+     * @param y It is the offset parameter on Y.
+     * @param z It is the offset parameter on Z.
+     * @param randomOffset Adds a random offset between 0 and the custom value.*/
+    public Offset(double x, double y, double z, double randomOffset) {
+        this.hasRandomOffset = true;
+        this.minRandomOffset = 0;
+        this.maxRandomOffset = randomOffset;
+
+        this.xOffset = random.nextDouble(maxRandomOffset);
+        this.yOffset = random.nextDouble(maxRandomOffset);
+        this.zOffset = random.nextDouble(maxRandomOffset);
+
+        this.x = x + xOffset;
+        this.y = y + yOffset;
+        this.z = z + zOffset;
+    }
+
+    /**@param x It is the offset parameter on X.
+     * @param y It is the offset parameter on Y.
+     * @param z It is the offset parameter on Z.
+     * @param minRandomOffset Adds a random offset starting from this value.
+     * @param maxRandomOffset Adds a random offset ending in this value.*/
+    public Offset(double x, double y, double z, double minRandomOffset, double maxRandomOffset) {
+
+        if(minRandomOffset > maxRandomOffset){
+            minRandomOffset = 0;
+            maxRandomOffset = 1;
+        }
+        this.hasRandomOffset = true;
+        this.minRandomOffset = minRandomOffset;
+        this.maxRandomOffset = maxRandomOffset;
+
+        this.xOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+        this.yOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+        this.zOffset = random.nextDouble(minRandomOffset,maxRandomOffset);
+
+        this.x = x + xOffset;
+        this.y = y + yOffset;
+        this.z = z + zOffset;
+    }
+
     /**@return Returns the offset on X (double).*/
     public double getX() {
-        if(hasRandomOffset){
-            return x + random.nextDouble(minRandomOffset,maxRandomOffset);
-        }
         return x;
     }
 
     /**@return Returns the offset on X.(int)*/
     public int getXInt() {
-        if(hasRandomOffset){
-            return x + random.nextInt((int) minRandomOffset, (int) maxRandomOffset);
-        }
-        return x;
+        return (int) x;
     }
 
     /**@return Returns the offset on Y.*/
     public double getY() {
-        if(hasRandomOffset){
-            return y + random.nextDouble(minRandomOffset,maxRandomOffset);
-        }
         return y;
     }
 
     /**@return Returns the offset on Y.(int)*/
     public int getYInt() {
-        if(hasRandomOffset){
-            return y + random.nextInt((int) minRandomOffset, (int) maxRandomOffset);
-        }
-        return y;
+        return (int) y;
     }
 
     /**@return Returns the offset on Z.*/
     public double getZ() {
-        if(hasRandomOffset){
-            return z + random.nextDouble(minRandomOffset,maxRandomOffset);
-        }
         return z;
     }
 
     /**@return Returns the offset on Z.(int)*/
     public int getZInt() {
-        if(hasRandomOffset){
-            return z + random.nextInt((int) minRandomOffset, (int) maxRandomOffset);
-        }
-        return z;
+        return (int) z;
     }
+
+    public boolean isHasRandomOffset() {
+        return hasRandomOffset;
+    }
+
+    public double getMinRandomOffset() {
+        return minRandomOffset;
+    }
+
+    public double getMaxRandomOffset() {
+        return maxRandomOffset;
+    }
+
+    public double getXOffset() {
+        return xOffset;
+    }
+
+    public double getYOffset() {
+        return yOffset;
+    }
+
+    public double getZOffset() {
+        return zOffset;
+    }
+
+    public int getXOffsetToInt() {
+        return (int) xOffset;
+    }
+
+    public int getYOffsetToInt() {
+        return (int) yOffset;
+    }
+
+    public int getZOffsetToInt() {
+        return (int) zOffset;
+    }
+
+    public BlockPos getBlockPos(){
+        return new BlockPos((int) x, (int) y, (int) z);
+    }
+
 }
