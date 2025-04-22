4e606bb8d3b812d6266d8e70943a8d059465e8ae
Badpig Studios <97912584+BadpigStudios@users.noreply.github.com>
2025-04-21 18:48:16 -0600

    - Update 0.1.2 * Render Layer Drawer(BETA - may be deleted) * A bunch of new utility classes * Fixes to existing code

--- src/main/java/baspig/apis/utils/Baspig_utils.java
+++ src/main/java/baspig/apis/utils/Baspig_utils.java
@@ -1,10 +1,8 @@
 package baspig.apis.utils;
 
 import baspig.apis.utils.advanced.audio.IsRegAudio;
-import baspig.apis.utils.commands.AddDrawer;
 import baspig.apis.utils.events.block.BlockEvents;
 import baspig.apis.utils.events.entity.EntityEvents;
-import baspig.apis.utils.util.vertex.StringRenderLayerHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -31,8 +29,6 @@ public class Baspig_utils implements ModInitializer {
         EntityEvents.RegisterEntityEvents(id);
         new IsRegAudio(id);
         BaspigsRegistries.reg();
-        new AddDrawer();
 
-        new StringRenderLayerHandler();
     }
 }
--- src/main/java/baspig/apis/utils/client/Baspig_utilsClient.java
+++ src/main/java/baspig/apis/utils/client/Baspig_utilsClient.java
@@ -1,18 +1,8 @@
 package baspig.apis.utils.client;
 
-import baspig.apis.utils.BaspigsRegistries;
-import baspig.apis.utils.client.render.draw_entity.DrawEntityRender;
-import baspig.apis.utils.client.render.draw_entity.edit.DrawEntityEditTool;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.debug.DebugRenderer;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,31 +15,6 @@ public class Baspig_utilsClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        /// Registers all the needed packets for data transfer
-        /// Registers the Draw Entity
-        EntityRendererRegistry.register(
-                BaspigsRegistries.DRAW_ENTITY,
-                DrawEntityRender::new);
-        /// Registers the client side screen for the Draw Entity
-        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
-            MinecraftClient client = MinecraftClient.getInstance();
-            PlayerEntity player = client.player;
-            if (player == null) return;
 
-            if (!(player.getMainHandStack().getItem() instanceof DrawEntityEditTool)) return;
-
-            HitResult hit = player.raycast(10.0D, context.tickCounter().getFixedDeltaTicks(), false);
-            if (hit.getType() == HitResult.Type.BLOCK) {
-                BlockHitResult blockHit = (BlockHitResult) hit;
-                BlockPos pos = blockHit.getBlockPos();
-
-                DebugRenderer.drawBox(
-                        context.matrixStack(),
-                        context.consumers(),
-                        pos, pos,
-                        1.0f, 1.0f, 1.0f, 0.5f
-                );
-            }
-        });
     }
 }
